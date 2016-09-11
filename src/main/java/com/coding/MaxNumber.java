52239f00d3851a9b948f5e7692922a263cd08f60
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-09-11 09:05:50 -0400

    Initial commit for a project with spock dependency

---
+++ src/main/java/com/coding/MaxNumber.java
@@ -0,0 +1,7 @@
+package com.coding;
+
+public class MaxNumber {
+    public int max(int numberOne, int numberTwo){
+        return Math.max(numberOne, numberTwo);
+    }
+}
---
+++ src/test/java/com/coding/MaxNumberSpec.groovy
@@ -0,0 +1,23 @@
+package com.coding
+
+import spock.lang.Specification
+
+class MaxNumberSpec extends Specification {
+    MaxNumber maxNumber = new MaxNumber()
+
+    def "test - max - valid"(){
+        given:
+        int numberOne = 12
+        int numberTwo = 15
+
+        when:
+        int maxNumber = maxNumber.max(numberOne, numberTwo)
+
+        then:
+        0 * _
+
+        and:
+        maxNumber == 15
+
+    }
+}
