d3a1f0ffc2433740a89e76cefc410063c07a1bfd
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-04-26 23:33:24 -0400

    Added class to demonstrate joining method of Collectors

---
+++ src/main/java/com/coding/java8/SampleStream.java
@@ -0,0 +1,23 @@
+package com.coding.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class SampleStream {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("David", "Craig", "Noah", "Matt");
+
+        //print the names uppercase , comma separated
+        System.out.println(
+                names.stream()
+                        .map(String::toUpperCase)
+                        .collect(joining(", ")));
+
+
+    }
+}
+
+
