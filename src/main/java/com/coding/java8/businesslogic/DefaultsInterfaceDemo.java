c35e41db1d3e644793375075ee6c5b0b8bc937d7
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-04-30 11:59:00 -0400

    Added class to demonstrate defaults usage in interface

---
+++ src/main/java/com/coding/java8/businesslogic/DefaultsInterfaceDemo.java
@@ -0,0 +1,20 @@
+package com.coding.java8.businesslogic;
+
+public interface DefaultsInterfaceDemo {
+
+    /*
+     * Implementation in an interface is possible
+     * by using default keyword
+     */
+    default void land(){
+        System.out.println("DefaultsInterfaceDemo::land");
+    }
+
+    default void turn() {
+        System.out.println("DefaultsInterfaceDemo::turn");
+    }
+
+    default void fly(){
+        System.out.println("DefaultsInterfaceDemo::fly");
+    }
+}
--- src/main/java/com/coding/java8/businesslogic/impl/SampleStream.java
+++ src/main/java/com/coding/java8/businesslogic/impl/SampleStream.java
@@ -1,4 +1,4 @@
-package com.coding.java8;
+package com.coding.java8.businesslogic.impl;
 
 import java.util.Arrays;
 import java.util.List;
