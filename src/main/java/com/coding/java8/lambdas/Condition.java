490d5a9a180f4f0588b1468a17471c541b2991f5
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-05-13 19:46:41 -0400

    Sorting a List using Lambda Expression

---
+++ src/main/java/com/coding/java8/lambdas/Condition.java
@@ -0,0 +1,7 @@
+package com.coding.java8.lambdas;
+
+import com.coding.java8.lambdas.data.Person;
+
+public interface Condition {
+    boolean evaluatePrintCondition(Person person);
+}
---
+++ src/main/java/com/coding/java8/lambdas/PersonSorter.java
@@ -0,0 +1,54 @@
+package com.coding.java8.lambdas;
+
+import com.coding.java8.lambdas.data.Person;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class PersonSorter {
+    public static void main(String[] args) {
+        List<Person> persons = Arrays.asList(
+                new Person("Sherlock", "Holmes", 21),
+                new Person("Charles", "Dickens", 26),
+                new Person("Lewis", "Carroll", 24),
+                new Person("Thomas", "Carlyle", 27)
+        );
+
+        // Sort list by last name
+
+        List<Person> sortedByLastNames = sortByLastName(persons);
+
+        // Sort list by last name using lambda expression
+        Collections.sort(persons, (p1, p2) -> p1.getLastName().compareTo(p2.getLastName()));
+
+        // Create a method that prints all name in list
+        // Create a method that prints all people that have last name beginning with C
+
+        System.out.println(" ###### People in myList ###### ");
+        printConditionally(persons, p -> true);
+        System.out.println(" ######  People Whose Name start with C ###### ");
+        printConditionally(sortedByLastNames, p -> p.getLastName().startsWith("C"));
+    }
+
+    private static void printConditionally(List<Person> persons, Condition condition) {
+        for(Person person: persons){
+            if(condition.evaluatePrintCondition(person)) {
+                System.out.println(person);
+            }
+        }
+    }
+
+    private static List<Person> sortByLastName(List<Person> persons) {
+        if(!persons.isEmpty()){
+            Collections.sort(persons, new Comparator<Person>() {
+                @Override
+                public int compare(Person o1, Person o2) {
+                    return o1.getLastName().compareTo(o2.getLastName());
+                }
+            });
+        }
+        return persons;
+    }
+}
