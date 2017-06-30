e23d336fd27ef3f94eb626012b211b888af668cd
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-06-29 22:46:02 -0400

    Added classes to demonstrate this usage in lambda expressions

---
+++ src/main/java/com/coding/java8/ThisInLambda.java
@@ -0,0 +1,29 @@
+package com.coding.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * demonstrates how this keyword is used in lambda expressions
+ */
+public class ThisInLambda {
+
+    private static ThisInLambda INSTANCE = new ThisInLambda();
+    private int aNumber = 100;
+    private static List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+    public void doSomething() {
+        numbers.forEach(x -> {
+                    System.out.println(x + this.aNumber);
+                    if (this == INSTANCE) {
+                        System.out.println("Within the lambda body this" +
+                                "just refers to enclosing object");
+                    }
+                }
+        );
+    }
+
+    public static void main(String[] args) {
+        INSTANCE.doSomething();
+    }
+}
