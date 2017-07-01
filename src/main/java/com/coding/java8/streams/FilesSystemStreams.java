7187886b8cc075c5d642326d60e62eb550008e39
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2017-07-01 17:15:26 -0400

    Added a class to list the top level files in a path using the File Stream approach

---
+++ src/main/java/com/coding/java8/streams/FilesSystemStreams.java
@@ -0,0 +1,23 @@
+package com.coding.java8.streams;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * This program lists the files under a Directory <Top Level>
+ */
+public class FilesSystemStreams {
+    public static void main(String[] args) {
+        String workingDirectory = "/Users/rajatbhatnagar/cisl";
+        Path workingDirPath = FileSystems.getDefault().getPath(workingDirectory);
+        try {
+            Files.list(workingDirPath).forEach(
+                            System.out::println
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
